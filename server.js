d088a10da2c28c24159b6251c2c3bd655018afc0
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-01-03 20:38:29 +0100

    done

--- server.js
+++ server.js
@@ -2,79 +2,74 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
-const Contact = require("./Models/Contact");
+const Contact = require("./models/Contact");
 const DBUSER = process.env.DBUSER;
 const DBPWD = process.env.DBPWD;
+
+app.use(express.json());
 mongoose
   .connect(
-    `mongodb+srv://${DBUSER}:${DBPWD}@mycluster.t5ajheo.mongodb.net/contact-app?retryWrites=true&w=majority`
+    `mongodb+srv://${DBUSER}:${DBPWD}@mycluster.gww0avb.mongodb.net/db-contacts?retryWrites=true&w=majority`
   )
-  .then(() => console.log("connected to DataBase"))
-  .catch((err) => {
-    console.log("error");
-    console.log(err);
-  });
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log(err));
 
-// add Contacts: POST
-app.post("/api/addContacts", (req, res) => {
-  Contact.insertMany([
-    {
-      FullName: "Mohamed Tlili",
-      Email: "Mohamed@hotmail.com",
-      PhoneNumber: 29572401,
-      Birthdate: "24/04/1995",
-    },
-    {
-      FullName: "Emna Ben Sassi",
-      Email: "Emna@hotmail.com",
-      PhoneNumber: 24567401,
-      Birthdate: "24/04/1990",
-    },
-    {
-      FullName: "Saida Jouini",
-      Email: "Saida@hotmail.com",
-      PhoneNumber: 96809302,
-      Birthdate: "24/04/1966",
-    },
-    {
-      FullName: "Fatma Jouini",
-      Email: "Fatma@hotmail.com",
-      PhoneNumber: 23948328,
-      Birthdate: "24/04/1999",
-    },
-  ]);
-  res.json({ message: "ok" });
+app.post("/add-contact", async (req, res) => {
+  try {
+    let { fullName, email, phone, birthDate, gender, desc } = req.body;
+    let newContact = new Contact({
+      fullName,
+      email,
+      phone,
+      gender,
+      birthDate,
+      desc,
+    });
+    await newContact.save();
+    res
+      .status(200)
+      .json({ status: true, message: "Contact was created successfully" });
+  } catch (error) {
+    if (error) {
+      console.log(error);
+    }
+    // res
+    //   .status(400)
+    //   .json({ status: false, error: error.errors["email"].message });
+  //   if (error.errors["fullName"]) {
+  //     return res
+  //       .status(401)
+  //       .json({ status: false, error: error.errors["fullName"].message });
+  //   }
+  //   if (error.errors["email"]) {
+  //     return res
+  //       .status(401)
+  //       .json({ status: false, error: error.errors["email"].message });
+  //   }
+  //   if (error.errors["phone"]) {
+  //     return res
+  //       .status(401)
+  //       .json({ status: false, error: error.errors["phone"].message });
+  //   }
+  //   if (error.errors["gender"]) {
+  //     return res
+  //       .status(401)
+  //       .json({ status: false, error: error.errors["gender"].message });
+  //   }
+  //   if (error.errors["desc"]) {
+  //     return res
+  //       .status(401)
+  //       .json({ status: false, error: error.errors["desc"].message });
+  //   }
+  //   if (error.errors["birthDate"]) {
+  //     return res
+  //       .status(401)
+  //       .json({ status: false, error: error.errors["birthDate"].message });
+  //   }
+  // }
 });
 
-// get Contacts: GET
-app.get("/api/getContacts", async (req, res) => {
-  const contacts = await Contact.find();
-  res.json({ data: contacts });
-});
-// get one contact by name  / The /
-app.get("/api/getNameContacts", async (req, res) => {
-  const contacts = await Contact.find({ FullName: /Mohamed/ });
-  res.json({ data: contacts });
-});
 
-// update :PUT
-app.put("/api/UpdateContacts", async (req, res) => {
-  const contacts = await Contact.findByIdAndUpdate(
-    "6595761f9c9cad71e11310b6",
-    {
-      FullName: "Sarra ",
-    },
-    { new: true }
-  );
-  res.json({ data: contacts });
-});
-
-
-// delete
-app.delete("/api/DeleteContacts", async (req, res) => {
-    const contacts = await Contact.deleteOne({ FullName: "Fatma Jouini" } );
-    res.json({ data: contacts });
-  });
 
 app.listen(5000, (err) => {
   if (err) throw err;
